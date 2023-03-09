9396633b780e0e28373959ae01a19c707ba38170
Manoj Pillay <manoj.pillay@mongodb.com>
2023-03-09 15:08:10 -0800

    Basic working of auto-complete.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import Layout, { siteTitle } from "../components/layout";
 import Head from "next/head";
 import files from "../data/files";
 
 const Home = () => {
-  const editorRef = useRef(null);
+  const monaco = useMonaco();
+  const editorRef = useRef(monaco);
   const [fileName, setFileName] = useState("basic.json");
   const file = files[fileName as string];
 
@@ -35,18 +36,57 @@ const Home = () => {
           height="60vh"
           width="60vh"
           path={file.name}
-          defaultLanguage={file.language}
+          language={file.language}
           defaultValue={file.value}
+          options={{ readOnly: false, cursorBlinking: "blink" }}
           theme="vs-dark"
-          onMount={(editor) => (editorRef.current = editor)}
+          onMount={handleEditorDidMount}
           onChange={handleEditorChange}
+          onValidate={handleValidate}
         />
-        <button onClick={handleValidate}> Validate. </button>
+        <button onClick={handleValidateButtonClick}> Validate. </button>
       </section>
     </Layout>
   );
 
-  function handleValidate() {
+  function handleEditorDidMount(
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: monaco
+  ) {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [
+        {
+          uri: "http://myserver/foo-schema.json", // id of the first schema
+          fileMatch: ["basic.json"], // associate with our model
+          schema: {
+            type: "object",
+            properties: {
+              p1: {
+                enum: ["v1", "v2"],
+              },
+              p2: {
+                $ref: "http://myserver/bar-schema.json", // reference the second schema
+              },
+            },
+          },
+        },
+        {
+          uri: "http://myserver/bar-schema.json", // id of the second schema
+          schema: {
+            type: "object",
+            properties: {
+              q1: {
+                enum: ["x1", "x2"],
+              },
+            },
+          },
+        },
+      ],
+    });
+    editorRef.current = editor;
+  }
+  function handleValidateButtonClick() {
     // @ts-ignore
     const editorJsonString = editorRef.current.getValue();
     const editorJson = JSON.parse(editorJsonString);
@@ -54,6 +94,17 @@ const Home = () => {
     alert(JSON.stringify(editorJson));
   }
 
+  function handleValidate(markers) {
+    // model markers
+    markers.forEach((marker) =>
+      console.log(
+        "(Line Number",
+        marker.startLineNumber,
+        `) ${marker.severity}: `,
+        marker.message
+      )
+    );
+  }
   function handleEditorChange(value: any, _: any) {
     console.log("current value is :", value);
   }
