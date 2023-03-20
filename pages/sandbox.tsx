5b1179a4fa51f7ef9446149b6c2ee58438e4be4e
Manoj Pillay <manoj.pillay@mongodb.com>
2023-03-20 10:45:20 -0700

    fontsize is integer not string

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -71,7 +71,7 @@ export default function Home({ allSchema }) {
           defaultValue={file.value}
           options={{
             cursorBlinking: "blink",
-            fontSize: "15",
+            fontSize: 15,
             roundedSelection: false,
             padding: {
               top: 10,
