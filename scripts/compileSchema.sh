216679488ebbdc02f538ade30c3e102380634587
Manoj Pillay <manoj.pillay@mongodb.com>
2023-04-04 11:25:00 -0700

    Added fullIndex compile check to pre-commit hook

--- scripts/compileSchema.sh
+++ scripts/compileSchema.sh
@@ -4,3 +4,8 @@ npx ajv compile \
   -s ../schema/index.json \
   -r "../schema/components/**/*.json" \
   --strict=false
+
+npx ajv compile \
+  -s ../schema/fullIndex.json \
+  -r "../schema/components/**/*.json" \
+  --strict=false
